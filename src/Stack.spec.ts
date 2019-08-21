d9ab945ebae961d406f52cd770c6a3d2c9c0c361
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-08-21 07:54:52 -0700

    added tests for pop and peek

--- src/Stack.spec.ts
+++ src/Stack.spec.ts
@@ -20,9 +20,41 @@ test('push pushes data onto the stack', t => {
 test('push pushes more data onto the stack', t => {
   const s = new Stack<string>();
 
-  s.push('this');
-  s.push('that');
+  s.push('this').push('that');
 
   t.is(s.length, 2);
   t.is(s.peek(), 'that');
+});
+
+test('push is fluent', t => {
+  const s = new Stack<string>();
+
+  t.assert(s.push('this') instanceof Stack);
+});
+
+test('pop returns the most recently pushed element', t => {
+  const s = new Stack<string>();
+
+  s.push('this').push('that');
+
+  t.is(s.length, 2);
+  t.is(s.pop(), 'that');
+  t.is(s.length, 1);
+});
+
+test('pop throws an error if empty stack', t => {
+  const s = new Stack<string>();
+
+  const error = t.throws(() => s.pop());
+  t.assert(error instanceof EmptyStackError);
+});
+
+test('peek returns a copy of the top of the stack', t => {
+  const s = new Stack<number[]>();
+
+  s.push([1, 2]).push([3, 4]);
+  const peek = s.peek();
+
+  t.deepEqual(peek, [3, 4]);
+  t.not(s.peek(), peek);
 });
