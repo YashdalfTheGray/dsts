cf7a7eeef048dd7cfb4ffebd70fbb396fd423a46
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-08-25 07:42:59 -0700

    added tests for dequeue

--- src/Queue.spec.ts
+++ src/Queue.spec.ts
@@ -16,3 +16,19 @@ test('enqueue puts stuff in the queue', t => {
   t.is(q.size, 1);
   t.is(q.peek(), 4);
 });
+
+test('dequeue pulls stuff out of the queue', t => {
+  const q = new Queue<number>();
+
+  q.enqueue(4).enqueue(3);
+
+  t.is(q.dequeue(), 4);
+  t.is(q.size, 1);
+});
+
+test('dequeue throws an error if there is nothing in the queue', t => {
+  const q = new Queue<number>();
+
+  const error = t.throws(() => q.dequeue());
+  t.assert(error instanceof EmptyQueueError);
+});
