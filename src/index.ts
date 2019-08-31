b9b2524e88c697a4d4ca23676820d85218107a0b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-08-31 10:42:25 -0700

    added the tree node as an export to the index

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import LinkedList from './LinkedList';
 import ListNode from './ListNode';
 import Queue from './Queue';
 import Stack from './Stack';
+import TreeNode from './TreeNode';
 
-export { HashTable, LinkedList, ListNode, Queue, Stack };
+export { HashTable, LinkedList, ListNode, Queue, Stack, TreeNode };
