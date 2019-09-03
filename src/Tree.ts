9b151043b07f03c7c698b6ff513688bfc878bc64
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-09-02 20:44:43 -0700

    added a private method to traverse the tree depth first

--- src/Tree.ts
+++ src/Tree.ts
@@ -12,4 +12,17 @@ export default class Tree<T> {
   constructor(data: T) {
     this.root = new TreeNode<T>(data, null, []);
   }
+
+  private traverseDFS(nodeCallback: (node: T) => void) {
+    if (this.root) {
+      (function dfs(currentNode) {
+        for (let i = 0, length = currentNode.children.length; i < length; i++) {
+          dfs(currentNode.children[i]);
+        }
+        nodeCallback(currentNode.data);
+      })(this.root);
+    } else {
+      throw new Error('Tree is empty');
+    }
+  }
 }
