95e1589d6b877974c1124123ff4bbb4c01edbc6f
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-09-03 09:49:40 -0700

    added the tree to the mainn export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import LinkedList from './LinkedList';
 import ListNode from './ListNode';
 import Queue from './Queue';
 import Stack from './Stack';
+import Tree from './Tree';
 import TreeNode from './TreeNode';
 
-export { HashTable, LinkedList, ListNode, Queue, Stack, TreeNode };
+export { HashTable, LinkedList, ListNode, Queue, Stack, Tree, TreeNode };
