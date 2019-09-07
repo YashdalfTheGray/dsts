9043e0ca9d3dfc4fe53ccc098eb0c0da4593eba1
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-09-07 11:17:41 -0700

    added some tests for size on treenode

--- src/TreeNode.spec.ts
+++ src/TreeNode.spec.ts
@@ -26,3 +26,31 @@ test('tree node get data returns a copy', t => {
   t.deepEqual(data, [2, 3]);
   t.not(node.data, data);
 });
+
+test('tree node size returns 1 for just itself', t => {
+  const node = new TreeNode<number>(1);
+
+  t.is(node.size, 1);
+});
+
+test('tree node size returns the number of nodes with children', t => {
+  const node = new TreeNode<number>(1);
+
+  node.children.push(...[1, 2, 3].map(n => new TreeNode<number>(n, node)));
+
+  t.is(node.size, 4);
+});
+
+test('tree node size returns the number of nodes with more descendants', t => {
+  const node = new TreeNode<number>(1);
+
+  const child1: TreeNode<number> = new TreeNode<number>(2, node);
+  const child2: TreeNode<number> = new TreeNode<number>(5, node);
+  child1.children.push(...[3, 4].map(n => new TreeNode<number>(n, child1)));
+  child2.children.push(
+    ...[6, 7, 8, 9].map(n => new TreeNode<number>(n, child2))
+  );
+  node.children.push(child1, child2);
+
+  t.is(node.size, 9);
+});
