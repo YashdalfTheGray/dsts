b6bb8913948b020d659dd6da5e880b82ba888e47
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-09-10 07:25:49 -0700

    added a single test for add using depth first

--- src/Tree.spec.ts
+++ src/Tree.spec.ts
@@ -13,3 +13,12 @@ test('can take an optional tree root node data', t => {
 
   t.assert(tr instanceof Tree);
 });
+
+test('can add nodes to the tree given a specific parent using depth first', t => {
+  const tr = new Tree<number>(1);
+
+  tr.add(2, 1);
+
+  t.is(tr.size, 2);
+  t.is(tr.search(n => n.data === 1)!.children[0].data, 2);
+});
