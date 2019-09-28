fb15aabca2f32e411dcbfc1ea6c2646662ef98b2
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-09-28 08:33:00 -0700

    added a compare function to bst

--- src/BinarySearchTree.ts
+++ src/BinarySearchTree.ts
@@ -1,9 +1,11 @@
 import BSTNode from './BSTNode';
 
+export type Comparator<T> = (a: T, b: T) => -1 | 0 | 1;
+
 export default class BinarySearchTree<T> {
   private root: BSTNode<T>;
 
-  constructor(data: T) {
+  constructor(data: T, private compare: Comparator<T>) {
     this.root = new BSTNode<T>(data, null);
   }
 }
