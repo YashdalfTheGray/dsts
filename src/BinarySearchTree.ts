2a4394f3e783ec2b762c083aa23fb0366d152480
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-09-28 20:15:14 -0700

    added a numeric comparator function

--- src/BinarySearchTree.ts
+++ src/BinarySearchTree.ts
@@ -2,6 +2,16 @@ import BSTNode from './BSTNode';
 
 export type Comparator<T> = (a: T, b: T) => -1 | 0 | 1;
 
+export const numericCompare: Comparator<number> = (a: number, b: number) => {
+  if (a < b) {
+    return -1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return 1;
+  }
+};
+
 export default class BinarySearchTree<T> {
   private root: BSTNode<T>;
 
